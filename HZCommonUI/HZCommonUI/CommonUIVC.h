32198c0e5cac097af337f173261fe35ad7e7a7fa
MaHaoZhe <411408809@qq.com>
2018-04-20 11:44:14 +0800

    podspec

---
+++ HZCommonUI/HZCommonUI/CommonUIVC.h
@@ -0,0 +1,13 @@
+//
+//  CommonUIVC.h
+//  HZCommonUI
+//
+//  Created by MaHaoZhe on 2018/4/20.
+//  Copyright © 2018年 HachiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonUIVC : UIViewController
+
+@end
